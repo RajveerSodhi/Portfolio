74c7337fd19027af83bca0b80197206ab358992f
RajveerSodhi <rajveersodhi03@gmail.com>
2024-04-20 17:56:07 -0700

    Logo Added

--- app/WebsiteHeaders/Header.tsx
+++ app/WebsiteHeaders/Header.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link';
-const pages = ["Home", "SL", "Icon Pack", "Contact"];
+const pages = ["Home", "SL", "Icons", "Contact"];
 
 export default function Header() {
     return (
         <div className="h-16 bg-black flex flex-row justify-between items-center">
+            <a href="/">
+                <img src="/portfolio_logo.svg" alt="logo" className="h-12 ml-8" />
+            </a>
+
             <ul className="absolute right-8 flex flex-row space-x-6">
                 {pages.map((page, index) => (
                     <li key={index} className="text-xl text-white hover:text-slate-300 font-semibold">
