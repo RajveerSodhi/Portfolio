9f74373a5777c939c427a80fd6d99c1a03a65961
RajveerSodhi <rajveersodhi03@gmail.com>
2024-04-28 23:35:01 -0700

    added coming soon page

---
+++ app/WebsitePages/ComingSoon.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+        <h1 className="text-4xl font-bold text-center text-slate-900 p-4">
+            Coming Soon!
+        </h1>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import HomePage from "./WebsitePages/Home";
 import SLPage from "./WebsitePages/SL";
 import IconPack from "./WebsitePages/Icons";
 import Contact from "./WebsitePages/Contact";
+import ComingSoon from "./WebsitePages/ComingSoon";
 
 import Header from "./WebsiteHeaders/Header";
 import Footer from "./WebsiteHeaders/Footer";
@@ -15,11 +16,12 @@ export default function Home() {
         <title>Rajveer Sodhi</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-slate-100">
+      <ComingSoon />
+      {/* <main className="bg-slate-100">
         <Header />
         <HomePage />
         <Footer />
-      </main>
+      </main> */}
     </>
   );
 }
