72a8b954169be9d0d3623a72e4f067285ac06d11
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-04 15:04:00 -0700

    Update description.tsx

--- app/WebsiteComponents/description.tsx
+++ app/WebsiteComponents/description.tsx
@@ -4,7 +4,10 @@ export default function Description() {
     return (
         <Container className="text-center">
             <p className="lead">
-                I am a software developer with a passion for creating innovative and efficient solutions to complex problems. I have experience in full-stack web development, mobile app development, and data analysis. I am always looking for new opportunities to learn and grow as a developer.
+                I am a fourth-year student at the University of British Columbia pursuing an Honours in Computer Science. I am highly interested in the domains of Human-Computer Interaction, Extended Reality, and Artificial Intelligence.
+            </p>
+            <p className="lead">
+                My research in HCI focuses on comparing unencumbered interaction techniques in head-mounted displays (HMDs). I am also exploring the principles of In-Vehicle Infotainment Systems. My ongoing research concerns unencumbered interaction techniques for HMDs that support user mobility.
             </p>
         </Container>
     );
