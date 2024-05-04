426f069eb29ada83592616bf3265b3c6d2336e2d
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-03 19:46:01 -0700

    added navbar

--- app/Icons/page.tsx
+++ app/Icons/page.tsx
@@ -1,6 +1,7 @@
 export default function Icons() {
     return (
         <>
+            <h1>Icons!</h1>
         </>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
-import Head from 'next/head';
-
-import HomePage from "./WebsitePages/Home";
-import SLPage from "./WebsitePages/SL";
-import IconPack from "./WebsitePages/Icons";
-import Contact from "./WebsitePages/Contact";
-import ComingSoon from "./WebsitePages/ComingSoon";
-
-import Header from "./WebsiteHeaders/Header";
-import Footer from "./WebsiteHeaders/Footer";
+import HomePage from "./Home";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Rajveer Sodhi</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* <ComingSoon /> */}
-      <main className="bg-slate-100">
-        <Header />
-        <HomePage />
-        <Footer />
-      </main>
+      <HomePage />
     </>
   );
 }
