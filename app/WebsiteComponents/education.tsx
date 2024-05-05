bccb6ba3f57033f2de30461d7a2a2436515fda0d
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-05 01:40:48 -0700

    Update education.tsx

--- app/WebsiteComponents/education.tsx
+++ app/WebsiteComponents/education.tsx
@@ -10,8 +10,9 @@ export default function Education() {
                         <h6 className="text-muted fw-bold">Batchelor of Science (Honours in Computer Science, Minor in Data Science)</h6>
                         <p className="text-muted mb-2">Kelowna, BC, Canada  |  2021 - 2026</p>
                         <hr className="mb-4" />
-                        <p className="text-muted mb-1"><strong>GPA: </strong>4.33</p>
-                        <p className="text-muted"><strong>Notable Coursework: </strong>Data Structures, Software Engineering, Machine Learning, Artificial Intelligence, Introduction to Data Analysis, Human-Computer Interaction, Database Systems</p>
+                        <p className="text-muted mb-2"><strong>GPA: </strong>4.33</p>
+                        <p className="text-muted mb-2"><strong>Notable Coursework: </strong>Data Structures, Software Engineering, Machine Learning, Artificial Intelligence, Introduction to Data Analysis, Human-Computer Interaction, Database Systems</p>
+                        <p className="text-muted"><strong>Awards: </strong>Dean's List (2021, 2022), Deputy Vice Chancellor's Scholarship for International Students (2021), CMPS Undergraduate Award Nomination (2021)</p>
                     </Card>
                 </Col>
 
@@ -21,8 +22,8 @@ export default function Education() {
                         <h6 className="text-muted fw-bold">High School Diploma, Central Board of Secondary Education</h6>
                         <p className="text-muted mb-2">Gurgaon, Haryana, India  |  2017 - 2021</p>
                         <hr className="mb-4" />
-                        <p className="text-muted mb-1"><strong>Final Grade: </strong>95.6%</p>
-                        <p className="text-muted"><strong>Coursework: </strong>English, Physics, Chemistry, Mathematics, Computer Science</p>
+                        <p className="text-muted mb-2"><strong>Grade: </strong>95.6%</p>
+                        <p className="text-muted mb-2"><strong>Coursework: </strong>English, Physics, Chemistry, Mathematics, Computer Science</p>
                     </Card>
                 </Col>
             </Row>
