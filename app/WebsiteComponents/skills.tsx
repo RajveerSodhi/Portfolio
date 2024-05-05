e4744ab45ac149fb35031d1af3652923895b6ccb
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-05 02:16:45 -0700

    added hover to skills

--- app/WebsiteComponents/skills.tsx
+++ app/WebsiteComponents/skills.tsx
@@ -1,3 +1,4 @@
+import "../static/skills.css"
 import { Row, Container, Card } from "react-bootstrap";
 
 export default function Skills() {
@@ -5,50 +6,50 @@ export default function Skills() {
         <Container className="text-center mt-4">
             <Row className="justify-content-center d-flex align-items-stretch mb-4">
                 <h3>Languages</h3>
-                <Card className="py-2 px-3 m-2 w-auto">Python</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Java</Card>
-                <Card className="py-2 px-3 m-2 w-auto">C#</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Kotlin</Card>
-                <Card className="py-2 px-3 m-2 w-auto">JavaScript</Card>
-                <Card className="py-2 px-3 m-2 w-auto">TypeScript</Card>
-                <Card className="py-2 px-3 m-2 w-auto">R</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Python</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Java</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">C#</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Kotlin</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">JavaScript</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">TypeScript</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">R</Card>
             </Row>
             <Row className="justify-content-center d-flex align-items-stretch mb-4">
                 <h3>Web Development</h3>
-                <Card className="py-2 px-3 m-2 w-auto">HTML</Card>
-                <Card className="py-2 px-3 m-2 w-auto">CSS</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Tailwind CSS</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Bootstrap</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Flask</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Next.js</Card>
-                <Card className="py-2 px-3 m-2 w-auto">React.js</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Node.js</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Express.js</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Axios</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Mongoose</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">HTML</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">CSS</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Tailwind CSS</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Bootstrap</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Flask</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Next.js</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">React.js</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Node.js</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Express.js</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Axios</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Mongoose</Card>
 
             </Row>
             <Row className="justify-content-center d-flex align-items-stretch mb-4">
                 <h3>Frameworks and Libraries</h3>
-                <Card className="py-2 px-3 m-2 w-auto">Pandas</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Seaborn</Card>
-                <Card className="py-2 px-3 m-2 w-auto">MatPlotLib</Card>
-                <Card className="py-2 px-3 m-2 w-auto">NumPy</Card>
-                <Card className="py-2 px-3 m-2 w-auto">JUnit</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Keras</Card>
-                <Card className="py-2 px-3 m-2 w-auto">OpenCV</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Selenium</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Pandas</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Seaborn</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">MatPlotLib</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">NumPy</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">JUnit</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Keras</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">OpenCV</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Selenium</Card>
             </Row>
             <Row className="justify-content-center d-flex align-items-stretch">
                 <h3>Tools</h3>
-                <Card className="py-2 px-3 m-2 w-auto">Unity</Card>
-                <Card className="py-2 px-3 m-2 w-auto">GitHub</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Tableau</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Power BI</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Firebase</Card>
-                <Card className="py-2 px-3 m-2 w-auto">Android Studio</Card>
-                <Card className="py-2 px-3 m-2 w-auto">MySQL</Card>
-                <Card className="py-2 px-3 m-2 w-auto">MongoDB</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Unity</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">GitHub</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Tableau</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Power BI</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Firebase</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">Android Studio</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">MySQL</Card>
+                <Card className="py-2 px-3 m-2 w-auto card-hover-shadow">MongoDB</Card>
             </Row>
         </Container>
     );
