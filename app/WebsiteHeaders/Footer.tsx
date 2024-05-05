19bce60cb182843a35d55a1f00627035955ee6e4
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-05 15:07:28 -0700

    added projects

--- app/WebsiteHeaders/Footer.tsx
+++ app/WebsiteHeaders/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <>
             <div className="bg-dark text-center p-3 mt-4">
-                <p className="text-white m-1">Last Updated: May 4, 2024</p>
+                <p className="text-white m-1">Last Updated: May 5, 2024</p>
             </div >
         </>
 
