7dde765dfff9c79eea6a566c2a4702a4168bec24
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-04 22:45:44 -0700

    added research

--- app/WebsiteComponents/education.tsx
+++ app/WebsiteComponents/education.tsx
@@ -5,20 +5,22 @@ export default function Education() {
         <Container className="w-100 my-5 text-center">
             <Row className="justify-content-center">
                 <Col xs={12} md={8} className="mb-4">
-                    <Card className="p-5 shadow" style={{ borderRadius: "15px" }}>
-                        <h5 className="fw-bold">University of British Columbia</h5>
+                    <Card className="p-4 shadow" style={{ borderRadius: "20px" }}>
+                        <h4 className="fw-bold mb-4">University of British Columbia</h4>
                         <h6 className="text-muted fw-bold">Batchelor of Science (Honours in Computer Science, Minor in Data Science)</h6>
-                        <p className="text-muted mb-4">Kelowna, BC, Canada  |  2021 - 2026</p>
+                        <p className="text-muted mb-2">Kelowna, BC, Canada  |  2021 - 2026</p>
+                        <hr className="mb-4" />
                         <p className="text-muted mb-1"><strong>GPA: </strong>4.33</p>
                         <p className="text-muted"><strong>Notable Coursework: </strong>Data Structures, Software Engineering, Machine Learning, Artificial Intelligence, Introduction to Data Analysis, Human-Computer Interaction, Database Systems</p>
                     </Card>
                 </Col>
 
                 <Col xs={12} md={8} className="mb-4">
-                    <Card className="p-5 shadow" style={{ borderRadius: "15px" }}>
-                        <h5 className="fw-bold">Heritage Xperiential Learning School</h5>
+                    <Card className="p-4 shadow" style={{ borderRadius: "20px" }}>
+                        <h4 className="fw-bold mb-4">Heritage Xperiential Learning School</h4>
                         <h6 className="text-muted fw-bold">High School Diploma, Central Board of Secondary Education</h6>
-                        <p className="text-muted mb-4">Gurgaon, Haryana, India  |  2017 - 2021</p>
+                        <p className="text-muted mb-2">Gurgaon, Haryana, India  |  2017 - 2021</p>
+                        <hr className="mb-4" />
                         <p className="text-muted mb-1"><strong>Final Grade: </strong>95.6%</p>
                         <p className="text-muted"><strong>Coursework: </strong>English, Physics, Chemistry, Mathematics, Computer Science</p>
                     </Card>
--- app/WebsiteComponents/research.tsx
+++ app/WebsiteComponents/research.tsx
@@ -1,6 +1,25 @@
+import { Container, Row, Col, Card } from "react-bootstrap";
+
 export default function Research() {
     return (
-        <>
-        </>
+        <Container className="w-100 my-5 text-center">
+            <Row className="justify-content-center">
+                <Col xs={12} md={4} className="mb-4">
+                    <Card className="p-4 shadow" style={{ borderRadius: "20px" }}>
+                        <h4 className="fw-bold mb-2">Comparison of Unencumbered Interaction Techniques in Head Mounted Displays</h4>
+                        <hr className="mb-4" />
+                        <h5 className="text-muted">In Review</h5>
+                    </Card>
+                </Col>
+
+                <Col xs={12} md={4} className="mb-4">
+                    <Card className="p-4 shadow" style={{ borderRadius: "20px" }}>
+                        <h4 className="fw-bold mb-2">Analysing Unencumbered Interaction Techniques in Head Mounted Displays while Walking</h4>
+                        <hr className="mb-4" />
+                        <h5 className="text-muted">In Progress</h5>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
