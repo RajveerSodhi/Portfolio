367da4033679ee0471351222a043aff02de6a156
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-05 17:03:17 -0700

    added project buttons

--- app/WebsiteComponents/projects.tsx
+++ app/WebsiteComponents/projects.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
             <Row className="justify-content-center">
                 <Col xs={12} md={8} className="mb-4">
                     <Card className="p-4 shadow card-radius">
-                        <h4 className="fw-bold mb-4"><a href="https://www.rajveersodhi.com" rel="noopener noreferrer" target="_blank" className="link">Personal Website</a></h4>
+                        <h4 className="fw-bold mb-4">Personal Website</h4>
                         <h6 className="text-muted fw-bold">Next.ts, Bootstrap, Tailwind CSS, TypeScript</h6>
                         <p className="text-muted mb-2">May 2024</p>
                         <hr className="mb-4" />
@@ -15,12 +15,13 @@ export default function Projects() {
                             <li>Created a personal website showcasing my technical skills, work experience, research, and more.</li>
                             <li>Developed using the Next.ts framework in TypeScript and styled with Bootstrap Tailwind CSS.</li>
                         </ul>
+                        <a className="btn btn-light project-link mt-3" rel="noopener noreferrer" target="_blank" href="https://github.com/RajveerSodhi/Portfolio">View on GitHub</a>
                     </Card>
                 </Col>
 
                 <Col xs={12} md={8} className="mb-4">
                     <Card className="p-4 shadow card-radius">
-                        <h4 className="fw-bold mb-4"><a href="https://github.com/RajveerSodhi/EduPool" rel="noopener noreferrer" target="_blank" className="link">EduPool: E-Learning Platform</a></h4>
+                        <h4 className="fw-bold mb-4">EduPool: E-Learning Platform</h4>
                         <h6 className="text-muted fw-bold">Python, HTML, CSS, JavaScript, SQL</h6>
                         <p className="text-muted mb-2">Apr 2024</p>
                         <hr className="mb-4" />
@@ -28,12 +29,13 @@ export default function Projects() {
                             <li>Collaborated in developing a Flask-based e-learning platform, employing Agile methodologies, CI/CD practices, UML for system design, and Test-Driven Development (TDD) with Selenium for automated testing and deployment to ensure quality and performance.</li>
                             <li>Implemented core features like user authentication, course management, discussion portals, assignment submissions, and grading.</li>
                         </ul>
+                        <a className="btn btn-light project-link mt-3" rel="noopener noreferrer" target="_blank" href="https://github.com/RajveerSodhi/EduPool">View on GitHub</a>
                     </Card>
                 </Col>
 
                 <Col xs={12} md={8} className="mb-4">
                     <Card className="p-4 shadow card-radius">
-                        <h4 className="fw-bold mb-4"><a href="https://github.com/RajveerSodhi/Gestura" rel="noopener noreferrer" target="_blank" className="link">Gestura: Sign Langauge to English Converter</a></h4>
+                        <h4 className="fw-bold mb-4">Gestura: Sign Langauge to English Converter</h4>
                         <h6 className="text-muted fw-bold">Python, HTML, CSS, JavaScript</h6>
                         <p className="text-muted mb-2">Jan 2024</p>
                         <hr className="mb-4" />
@@ -42,12 +44,13 @@ export default function Projects() {
                             <li>Led a cross-functional team in developing a full-stack website, using Node.js to translate American Sign Language into English, integrating a gesture-recognition machine learning model developed with Keras and OpenCV.</li>
                             <li>Implemented the Google Translate and Text-to-Speech API to enhance the project's capabilities by generating multilingual text and speech outputs for the translations.</li>
                         </ul>
+                        <a className="btn btn-light project-link mt-3" rel="noopener noreferrer" target="_blank" href="https://github.com/RajveerSodhi/Gestura">View on GitHub</a>
                     </Card>
                 </Col>
 
                 <Col xs={12} md={8} className="mb-4">
                     <Card className="p-4 shadow card-radius">
-                        <h4 className="fw-bold mb-4"><a href="https://github.com/RajveerSodhi/TransitMate" rel="noopener noreferrer" target="_blank" className="link">TransitMate: Ride Sharing App Prototype</a></h4>
+                        <h4 className="fw-bold mb-4">TransitMate: Ride Sharing App Prototype</h4>
                         <h6 className="text-muted fw-bold">Java, XML</h6>
                         <p className="text-muted mb-2">Dec 2023</p>
                         <hr className="mb-4" />
@@ -56,6 +59,7 @@ export default function Projects() {
                             <li>Applied HCI fundamentals by conducting multiple rounds of requirements gathering and user testing to refine the user experience.</li>
                             <li>Utilized object-oriented principles to ensure efficient code organization, enhancing maintainability and scalability of the app.</li>
                         </ul>
+                        <a className="btn btn-light project-link mt-3" rel="noopener noreferrer" target="_blank" href="https://github.com/RajveerSodhi/TransitMate">View on GitHub</a>
                     </Card>
                 </Col>
             </Row>
