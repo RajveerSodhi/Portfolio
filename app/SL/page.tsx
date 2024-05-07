3605257e98b46158d4e8a0f173ed2b90d92535e5
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-07 13:11:41 -0700

    adjusted margins

--- app/SL/page.tsx
+++ app/SL/page.tsx
@@ -1,7 +1,7 @@
 export default function SL() {
     return (
         <>
-            <h1 className="text-center m-4" style={{ marginTop: "8rem" }}>Supplemental Learning Material: Coming Soon!</h1>
+            <h1 className="text-center" style={{ marginTop: "8rem" }}>Supplemental Learning Material: Coming Soon!</h1>
         </>
     );
 }
