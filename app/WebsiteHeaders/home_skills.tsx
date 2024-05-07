6c5b9174065cb70507e96fce7d51cdcd27eb1ad7
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-07 03:09:49 -0700

    added collapsable headers

--- app/WebsiteHeaders/home_skills.tsx
+++ app/WebsiteHeaders/home_skills.tsx
@@ -1,11 +1,23 @@
+"use client";
+import { useState } from "react";
 import { Container } from "react-bootstrap";
-import Skills from "../WebsiteComponents/skills"
+import Skills from "../WebsiteComponents/skills";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 export default function HomeSkills() {
+    const [skillsVisible, setSkillsVisible] = useState(false);
+
+    const toggleSkillsVisibility = () => {
+        setSkillsVisible(!skillsVisible);
+    };
+
     return (
         <Container style={{ marginBottom: "4rem" }}>
-            <h2 className="text-center display-4 mb-4">Skills</h2>
-            <Skills />
+            <h2 className="text-center display-4 home-header" onClick={toggleSkillsVisibility}>
+                Skills {skillsVisible ? <MdKeyboardArrowDown /> : <MdKeyboardArrowRight />}
+            </h2>
+            {skillsVisible && <Skills />}
         </Container>
     );
 }
--- app/WebsiteHeaders/home_workex.tsx
+++ app/WebsiteHeaders/home_workex.tsx
@@ -1,11 +1,23 @@
+"use client";
+import { useState } from "react";
 import { Container } from "react-bootstrap";
-import WorkEx from "../WebsiteComponents/workex"
+import WorkEx from "../WebsiteComponents/workex";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
-export default function HomeWorkex() {
+export default function HomeWorkEx() {
+    const [workexVisible, setWorkExVisible] = useState(false);
+
+    const toggleWorkExVisibility = () => {
+        setWorkExVisible(!workexVisible);
+    };
+
     return (
         <Container style={{ marginBottom: "4rem" }}>
-            <h2 className="text-center display-4">Work Experience</h2>
-            <WorkEx />
+            <h2 className="text-center display-4 home-header" onClick={toggleWorkExVisibility}>
+                Work Experience {workexVisible ? <MdKeyboardArrowDown /> : <MdKeyboardArrowRight />}
+            </h2>
+            {workexVisible && <WorkEx />}
         </Container>
     );
 }
