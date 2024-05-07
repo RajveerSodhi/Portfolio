0766d8ef747d9c51b58c4fec21a7334a38ff97b4
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-07 03:25:27 -0700

    bug fixes

--- app/WebsiteComponents/project_button.tsx
+++ app/WebsiteComponents/project_button.tsx
@@ -1,7 +1,7 @@
-import { FaArrowRight } from "react-icons/fa";
+import { MdOpenInNew } from "react-icons/md";
 
 export default function ProjectButton(props: { link: string }) {
     return (
-        <a className="btn btn-light project-link mt-3 p-2" rel="noopener noreferrer" target="_blank" href={`https://github.com/RajveerSodhi/${props.link}`}> View on GitHub < FaArrowRight /></a >
+        <a className="btn btn-light project-link mt-3 p-2" rel="noopener noreferrer" target="_blank" href={`https://github.com/RajveerSodhi/${props.link}`}> View on GitHub <MdOpenInNew /></a >
     );
 }
