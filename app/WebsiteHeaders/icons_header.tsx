4217b3aaf6fc1b9325de75d3688594375d64f122
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-10 18:26:34 -0700

    added bnw logo in icons page

--- app/WebsiteHeaders/icons_header.tsx
+++ app/WebsiteHeaders/icons_header.tsx
@@ -1,5 +1,9 @@
 export default function IconsHeader() {
     return (
-        <h1 className="display-1 mb-4" > Minimal iOS Icon Pack </h1>
+        <>
+            <h1 className="display-1 mb-4" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <img src="logo_bnw.svg" alt="Rajveer Sodhi Logo Badge" style={{ display: "inline-block", height: "0.75em", marginRight: "0.4em" }} /> Minimal iOS Icon Pack
+            </h1>
+        </>
     );
 }
