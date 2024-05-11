5d381c410186930c884e834319122801aac057a9
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-10 18:31:41 -0700

    header edit

--- app/WebsiteHeaders/icons_header.tsx
+++ app/WebsiteHeaders/icons_header.tsx
@@ -2,7 +2,8 @@ export default function IconsHeader() {
     return (
         <>
             <h1 className="display-1 mb-4" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                <img src="logo_bnw.svg" alt="Rajveer Sodhi Logo Badge" style={{ display: "inline-block", height: "0.75em", marginRight: "0.4em" }} /> Minimal iOS Icon Pack
+                <img src="logo_bnw.svg" alt="Rajveer Sodhi Logo Badge" style={{ display: "inline-block", height: "0.75em", marginRight: "0.5em" }} />
+                iOS Icon Pack
             </h1>
         </>
     );
