22b7cfa0023b23c0a0c6a5360995a68364839faa
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-15 13:44:14 -0700

    Update DownloadButton.tsx

--- app/WebsiteComponents/IconsComponents/PaymentForm/DownloadButton.tsx
+++ app/WebsiteComponents/IconsComponents/PaymentForm/DownloadButton.tsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 export default function DownloadButton() {
     return (
         <Container className="download">
-            <Link download href="./icons.zip" className="download-button">Dowload Now</Link>
+            <a download href="/icons.zip" className="download-button">Dowload Now</a>
         </Container>
     );
 }
