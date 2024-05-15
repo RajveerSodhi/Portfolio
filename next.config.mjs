bd4ad69a9da6edcf71175af902bef67c8076da3b
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-15 01:30:04 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
