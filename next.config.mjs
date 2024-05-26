258a3334499c9c7c2e16d084036334dde6c9e6dc
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-25 22:50:12 -0700

    bug fixing

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,19 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' https://js.stripe.com; style-src 'self' 'unsafe-inline'; frame-src https://js.stripe.com; img-src 'self'; connect-src 'self' https://api.stripe.com",
-                    },
-                ],
-            },
-        ];
-    },
+    // async headers() {
+    //     return [
+    //         {
+    //             source: '/(.*)',
+    //             headers: [
+    //                 {
+    //                     key: 'Content-Security-Policy',
+    //                     value: "default-src 'self'; script-src 'self' 'unsafe-inline' https://js.stripe.com; style-src 'self' 'unsafe-inline'; frame-src https://js.stripe.com; img-src 'self'; connect-src 'self' https://api.stripe.com",
+    //                 },
+    //             ],
+    //         },
+    //     ];
+    // },
 };
 
 export default nextConfig;
