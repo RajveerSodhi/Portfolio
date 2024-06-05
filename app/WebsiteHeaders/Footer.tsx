972ac445fc7d84d45b280a9cd80feb29245f93dd
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-04 20:51:33 -0700

    Update Footer.tsx

--- app/WebsiteHeaders/Footer.tsx
+++ app/WebsiteHeaders/Footer.tsx
@@ -1,8 +1,9 @@
 export default function Footer() {
     return (
         <>
-            <div className="bg-dark text-center p-3 mt-4">
-                <p className="text-white m-1">Last Updated: June 4, 2024</p>
+            <div className="bg-dark p-3 mt-4 text-white d-flex justify-content-between">
+                <span>Last Updated: June 4, 2024</span>
+                <span>Menu</span>
             </div >
         </>
 
