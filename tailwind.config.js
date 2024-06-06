5a691ad0095249c6a6207e023589509c9a656748
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-06 12:16:53 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
       fontFamily: {
         sans: ['var(--font-point)']
       },
+      colors: {
+        mywhite: "#f5f5f5",
+        myblack: "#27272A",
+      }
     },
   },
   plugins: [],
