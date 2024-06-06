f89778b121de9d6c1937dba3f4b85b8e8202e480
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-06 12:18:04 -0700

    removed description

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
 import Research from "./WebsiteHeaders/HomeHeaders/home_research";
 import Skills from "./WebsiteHeaders/HomeHeaders/home_skills";
-import Header from "./WebsiteHeaders/HomeHeaders/home_header"
-import Resume from "./WebsiteHeaders/HomeHeaders/home_resume"
-import Description from "./WebsiteHeaders/HomeHeaders/home_description";
+import Header from "./WebsiteHeaders/HomeHeaders/home_header";
+import Resume from "./WebsiteHeaders/HomeHeaders/home_resume";
 import Education from "./WebsiteHeaders/HomeHeaders/home_education";
 import WorkEx from "./WebsiteHeaders/HomeHeaders/home_workex";
 import Projects from "./WebsiteHeaders/HomeHeaders/home_projects";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Description />
-      <Education />
-      <WorkEx />
-      <Projects />
-      <Research />
-      <Skills />
-      <Resume />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Education />
+            <WorkEx />
+            <Projects />
+            <Research />
+            <Skills />
+            <Resume />
+        </>
+    );
 }
