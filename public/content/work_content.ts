41c2d3c58522f3966d8fa2ffd285e87c3f1e4bbb
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-06 23:30:22 -0700

    added projct and work pictures

--- public/content/work_content.ts
+++ public/content/work_content.ts
@@ -4,6 +4,7 @@ export const work = [
         company: "Data POEM",
         location: "Santa Clara, California, USA (Remote)",
         date: "May 2024 - Present",
+        img: "datapoem.png",
         description: [
             "Build connector APIs using FastAPI in Python for PostgreSQL to retrieve and transform website analytics data from various sources for an ML modelling pipeline.",
         ]
@@ -13,6 +14,7 @@ export const work = [
         company: "University of British Columbia",
         location: "Kelowna, BC",
         date: "Sep 2023 - Present",
+        img: "ubc.png",
         description: [
             "Conducted a study in Human-Computer Interaction (HCI) with 20 participants to compare cutting-edge command selection techniques using head-mounted displays, such as Hand-Proximate User Interface (HPUI) and gaze-tracked input.",
             "Explored the principles of In-Vehicle Infotainment Systems and built implementations of HPUI with C# and the Unity XR Toolkit.",
@@ -24,6 +26,7 @@ export const work = [
         company: "Niteo Africa",
         location: "Kelowna, BC",
         date: "Sep 2023 - Present",
+        img: "niteo.png",
         description: [
             "Administered Niteo Africa's WordPress-based website, incorporating blog posts and event information and diligently updating website plugins, resulting in a 150% increase in website viewership since assuming the role.",
             "Communicated concerns and insights to the Executive Director through detailed monthly reports to ensure regular progress."
@@ -34,6 +37,7 @@ export const work = [
         company: "University of British Columbia",
         location: "Kelowna, BC",
         date: "Sep 2023 - Dec 2023",
+        img: "ubc.png",
         description: [
             "Spearheaded the development of a module within a graphical Decision Support System in Java for long-term degree planning, which is expected to help over 10,000 students plan courses and streamline discussions with academic advisors.",
             "Added support for over 120 complex degree configurations and used Google CP-SAT Solver to build an optimization model for an optimal plan, along with various code refactoring and improvements to the optimizer."
