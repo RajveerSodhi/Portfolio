fbe374ed12537171e7824674c0640b3c7992828e
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-06 20:36:33 -0700

    started projects

--- app/Components/HomeComponents/Proj/projects.tsx
+++ app/Components/HomeComponents/Proj/projects.tsx
@@ -1,15 +1,13 @@
-import { Container, Row } from "react-bootstrap";
 import ProjectComponent from "./project_component";
 import { projects } from "../../../../public/content/projects_content";
 
 export default function Projects() {
     return (
-        <Container className="w-100 my-5 text-center">
-            <Row className="justify-content-center">
-                {projects.map((project, index) => (
-                    <ProjectComponent key={index} {...project} />
-                ))}
-            </Row>
-        </Container>
+        <section>
+            <h1>Projects</h1>
+            {projects.map((project, index) => (
+                <ProjectComponent key={index} {...project} />
+            ))}
+        </section>
     );
 }
