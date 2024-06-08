028b8d900124a3e9b6ab4995400b233b3784088c
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-08 14:37:42 -0700

    added projects to grid

--- app/Components/HomeComponents/Proj/projects.tsx
+++ app/Components/HomeComponents/Proj/projects.tsx
@@ -5,9 +5,11 @@ export default function Projects() {
     return (
         <section>
             <h1 className="mb-4 mt-12">Projects</h1>
-            {projects.map((project, index) => (
-                <ProjectComponent key={index} {...project} />
-            ))}
+            <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-4 mx-4">
+                {projects.map((project, index) => (
+                    <ProjectComponent key={index} {...project} />
+                ))}
+            </div>
         </section>
     );
 }
