e785c27011b24565a487b91605362cc501ad5821
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-08 14:37:53 -0700

    made proeject components responsive

--- app/Components/HomeComponents/Proj/project_component.tsx
+++ app/Components/HomeComponents/Proj/project_component.tsx
@@ -19,7 +19,7 @@ export default function ProjectComponent(props: {
 
     return (
         <Tilt tiltReverse={false} tiltMaxAngleX={1} tiltMaxAngleY={1}>
-            <div className="p-4 flex flex-col text-center items-center shadow-lg bg-[#fefefe] dark:bg-[#252525] border-1 border-solid border-[#e7e7e7] dark:border-[#454545] text-myblack dark:text-mywhite rounded-3xl w-[30rem] m-3 hover:scale-[1.01] duration-500 transition-transform ease-in-out">
+            <div className="p-4 flex flex-col text-center items-center shadow-lg bg-[#fefefe] dark:bg-[#252525] border-1 border-solid border-[#e7e7e7] dark:border-[#454545] text-myblack dark:text-mywhite rounded-3xl max-w-[29rem] w-auto hover:scale-[1.01] duration-500 transition-transform ease-in-out">
                 <Image
                     src={`/images/project_photos/thumbnail/${link}.png`}
                     alt={`Project Image - ${title}`}
@@ -29,17 +29,23 @@ export default function ProjectComponent(props: {
                     className="mb-4 select-none min-w-full aspect-auto shadow-lg rounded-t-2xl rounded-b-md object-cover cursor-pointer hover:scale-[1.01] duration-500 transition-transform ease-in-out"
                 />
                 <Lightbox openImage={openImage} setOpenImage={setOpenImage} link={link} />
-                <h2 className="mb-2">{title}</h2>
-                <h5 className="text-center mb-2 text-myblack/85 dark:text-mywhite/85">
-                    {subtitle}
-                </h5>
-                <h6 className="text-myblack/75 dark:text-mywhite/75">{tech}</h6>
-                <div className="text-myblack dark:text-mywhite mt-3 mb-1 text-left text-lg">
-                    {description.map((item, index) => (
-                        <li className="mb-3">{item}</li>
-                    ))}
+                <div id="project-title">
+                    <h2 className="mb-2">{title}</h2>
+                    <h5 className="text-center mb-2 text-myblack/85 dark:text-mywhite/85">
+                        {subtitle}
+                    </h5>
                 </div>
-                <ProjectButton link={link} />
+                <div className="hidden" id="project-description">
+                    <h6 className="text-myblack/75 dark:text-mywhite/75">{tech}</h6>
+                    <div className="text-myblack dark:text-mywhite mt-3 mb-1 text-left text-lg">
+                        {description.map((item, index) => (
+                            <li className="mb-3" key={index}>
+                                {item}
+                            </li>
+                        ))}
+                    </div>
+                    <ProjectButton link={link} />
+                </div>
             </div>
         </Tilt>
     );
