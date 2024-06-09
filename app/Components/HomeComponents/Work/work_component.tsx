efa150749c3fbefeaf1d12d962b3b4d6eb6b363a
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-09 00:36:02 -0700

    made workex responsive

--- app/Components/HomeComponents/Work/work_component.tsx
+++ app/Components/HomeComponents/Work/work_component.tsx
@@ -6,13 +6,14 @@ import WorkImage from "./work_image";
 
 export default function WorkComponent(props: {
     company: string;
+    tld: string;
     position: string;
     date: string;
     img: string;
     location: string;
     description: string[];
 }) {
-    const { company, position, date, img, location, description } = props;
+    const { company, position, tld, date, img, location, description } = props;
 
     return (
         <Tilt
@@ -26,34 +27,38 @@ export default function WorkComponent(props: {
             glareMaxOpacity={0.05}
             glareBorderRadius="1.5rem"
         >
-            <AccordionItem
-                className="hover:scale-[1.01] transition-transform duration-500 ease-in-out mt-[2.5rem] min-w-full w-auto flex flex-col justify-center items-center"
-                header={
-                    <div className="flex items-center p-4 text-myblack dark:text-mywhite rounded-3xl bg-[#fefefe] shadow-md hover:shadow-lg transition-shadow duration-500 ease-in-out dark:bg-[#252525] w-full mb-[-1rem] border-1 border-solid border-[#e7e7e7] dark:border-[#454545]">
-                        <WorkImage img={img} company={company} />
-                        <div className="ml-8 text-left">
-                            <h2 className="text-3xl">{position}</h2>
-                            <h2 className="text-xl text-zinc-600 dark:text-zinc-300">{company}</h2>
+            <div className="hover:scale-[1.01] transition-transform duration-500 ease-in-out mt-[2.5rem] min-w-full w-auto flex flex-col justify-center items-center">
+                <AccordionItem
+                    className="w-full"
+                    header={
+                        <div className="flex items-center p-4 text-myblack dark:text-mywhite rounded-3xl bg-[#fefefe] shadow-md hover:shadow-lg transition-shadow duration-500 ease-in-out dark:bg-[#252525] w-full mb-[-1rem] border-1 border-solid border-[#e7e7e7] dark:border-[#454545]">
+                            <WorkImage img={img} company={company} />
+                            <div className="ml-8 text-left">
+                                <h2 className="text-3xl">{position}</h2>
+                                <h2 className="text-xl text-zinc-600 dark:text-zinc-300">
+                                    {company}
+                                </h2>
+                            </div>
+                        </div>
+                    }
+                >
+                    <div className="px-6 pt-10 pb-3 bg-[#e7e7e7] dark:bg-[#454545] rounded-b-3xl">
+                        {description.map((desc, index) => (
+                            <li key={index} className="mb-3">
+                                {desc}
+                            </li>
+                        ))}
+                        <div className="text-zinc-500 dark:text-zinc-400 flex justify-between mt-4 mb-2">
+                            <span className="hover:text-myblack dark:hover:text-mywhite transition-color duration-500 ease-in-out">
+                                {location}
+                            </span>
+                            <span className="hover:text-myblack dark:hover:text-mywhite transition-color duration-500 ease-in-out">
+                                {date}
+                            </span>
                         </div>
                     </div>
-                }
-            >
-                <div className="px-6 pt-10 pb-3 bg-[#e7e7e7] dark:bg-[#454545] rounded-b-3xl">
-                    {description.map((desc, index) => (
-                        <li key={index} className="mb-3">
-                            {desc}
-                        </li>
-                    ))}
-                    <div className="text-zinc-500 dark:text-zinc-400 flex justify-between mt-4 mb-2">
-                        <span className="hover:text-myblack dark:hover:text-mywhite transition-color duration-500 ease-in-out">
-                            {location}
-                        </span>
-                        <span className="hover:text-myblack dark:hover:text-mywhite transition-color duration-500 ease-in-out">
-                            {date}
-                        </span>
-                    </div>
-                </div>
-            </AccordionItem>
+                </AccordionItem>
+            </div>
         </Tilt>
     );
 }
