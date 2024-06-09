d2d01d4ff466f0cf99bf7ebc47879dde2ca400ca
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-08 23:20:00 -0700

    Update work_component.tsx

--- app/Components/HomeComponents/Work/work_component.tsx
+++ app/Components/HomeComponents/Work/work_component.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { AccordionItem } from "@szhsin/react-accordion";
 import Tilt from "react-parallax-tilt";
+import WorkImage from "./work_image";
 
 export default function WorkComponent(props: {
     company: string;
@@ -28,15 +29,7 @@ export default function WorkComponent(props: {
                 <AccordionItem
                     header={
                         <div className="flex items-center p-4 text-myblack dark:text-mywhite rounded-3xl bg-[#fefefe] shadow-md hover:shadow-lg transition-shadow duration-500 ease-in-out dark:bg-[#252525] w-full min-w-[60rem] mb-[-1rem] border-1 border-solid border-[#e7e7e7] dark:border-[#454545] shadow-md">
-                            <div className="bg-mywhite w-auto h-auto min-w-[90px] min-h-[90px] aspect-square p-1 rounded-full flex items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-all ease-in-out duration-500">
-                                <Image
-                                    src={`/images/work_photos/${img}.png`}
-                                    alt={company}
-                                    width={40}
-                                    height={40}
-                                    className="min-h-[40px] min-w-[40px] object-cover"
-                                />
-                            </div>
+                            <WorkImage img={img} company={company} />
                             <div className="ml-8 text-left">
                                 <h2 className="text-3xl">{position}</h2>
                                 <h2 className="text-xl text-zinc-600 dark:text-zinc-300">
