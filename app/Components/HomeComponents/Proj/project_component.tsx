0f17e35b856818769f8488db44816be17a894bc5
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-09 00:23:42 -0700

    margin fix for projects

--- app/Components/HomeComponents/Proj/project_component.tsx
+++ app/Components/HomeComponents/Proj/project_component.tsx
@@ -18,54 +18,49 @@ export default function ProjectComponent(props: {
 
     return (
         <Tilt tiltReverse={false} tiltMaxAngleX={1} tiltMaxAngleY={1} gyroscope={true}>
-            <div className="flex flex-col text-center items-center shadow-lg bg-[#fefefe] dark:bg-[#252525] border-1 border-solid border-[#e7e7e7] dark:border-[#454545] text-myblack dark:text-mywhite rounded-3xl max-w-[29rem] w-auto hover:scale-[1.01] duration-500 transition-transform ease-in-out">
-                <AccordionItem
-                    header={
-                        <div className="p-4">
-                            <Tilt
-                                tiltReverse={false}
-                                tiltMaxAngleX={2}
-                                tiltMaxAngleY={2}
-                                gyroscope={true}
-                                glareEnable={true}
-                                glareReverse={true}
-                                glareMaxOpacity={0.2}
-                                glareBorderRadius="1rem"
-                            >
-                                <Image
-                                    src={`/images/project_photos/thumbnail/${link}.png`}
-                                    alt={`Project Image - ${title}`}
-                                    width={400}
-                                    height={400}
-                                    onClick={() => setOpenImage(true)}
-                                    className="mb-4 select-none min-w-full aspect-auto shadow-md hover:shadow-xl rounded-t-2xl rounded-b-md object-cover hover:scale-[1.01] duration-500 transition-all ease-in-out"
-                                />
-                            </Tilt>
-                            <Lightbox
-                                openImage={openImage}
-                                setOpenImage={setOpenImage}
-                                link={link}
+            <AccordionItem
+                className="flex flex-col text-center items-center shadow-lg bg-[#fefefe] dark:bg-[#252525] border-1 border-solid border-[#e7e7e7] dark:border-[#454545] text-myblack dark:text-mywhite rounded-3xl max-w-[29rem] w-auto hover:scale-[1.01] duration-500 transition-transform ease-in-out"
+                header={
+                    <div className="p-4">
+                        <Tilt
+                            tiltReverse={false}
+                            tiltMaxAngleX={2}
+                            tiltMaxAngleY={2}
+                            gyroscope={true}
+                            glareEnable={true}
+                            glareReverse={true}
+                            glareMaxOpacity={0.2}
+                            glareBorderRadius="1rem"
+                        >
+                            <Image
+                                src={`/images/project_photos/thumbnail/${link}.png`}
+                                alt={`Project Image - ${title}`}
+                                width={400}
+                                height={400}
+                                onClick={() => setOpenImage(true)}
+                                className="mb-4 select-none min-w-full aspect-auto shadow-md hover:shadow-xl rounded-t-2xl rounded-b-md object-cover hover:scale-[1.01] duration-500 transition-all ease-in-out"
                             />
-                            <h2 className="mb-2">{title}</h2>
-                            <h5 className="text-center mb-2 text-myblack/85 dark:text-mywhite/85">
-                                {subtitle}
-                            </h5>
-                        </div>
-                    }
-                >
-                    <div id="project-description" className="p-4 max-w-[448px]">
-                        <h6 className="text-myblack/75 dark:text-mywhite/75">{tech}</h6>
-                        <div className="text-myblack dark:text-mywhite mt-3 mb-1 text-left text-lg">
-                            {description.map((item, index) => (
-                                <li className="mb-3" key={index}>
-                                    {item}
-                                </li>
-                            ))}
-                        </div>
-                        <ProjectButton link={link} />
+                        </Tilt>
+                        <Lightbox openImage={openImage} setOpenImage={setOpenImage} link={link} />
+                        <h2 className="mb-2">{title}</h2>
+                        <h5 className="text-center mb-2 text-myblack/85 dark:text-mywhite/85">
+                            {subtitle}
+                        </h5>
                     </div>
-                </AccordionItem>
-            </div>
+                }
+            >
+                <div id="project-description" className="p-4 max-w-[448px]">
+                    <h6 className="text-myblack/75 dark:text-mywhite/75">{tech}</h6>
+                    <div className="text-myblack dark:text-mywhite mt-3 mb-1 text-left text-lg">
+                        {description.map((item, index) => (
+                            <li className="mb-3" key={index}>
+                                {item}
+                            </li>
+                        ))}
+                    </div>
+                    <ProjectButton link={link} />
+                </div>
+            </AccordionItem>
         </Tilt>
     );
 }
