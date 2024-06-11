eb0b62a22250c3199281bd1952f4fed54b602edc
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-10 19:27:46 -0700

    Update section_1.tsx

--- app/Components/IconsComponents/section_1.tsx
+++ app/Components/IconsComponents/section_1.tsx
@@ -2,26 +2,19 @@ import IPhone from "./iPhone";
 
 export default function Section1() {
     return (
-        <section>
-            <div style={{ width: "100%", maxWidth: "80%" }}>
-                <div className="d-flex align-items-center justify-content-center">
-                    <IPhone />
-                </div>
-                <div className="d-flex align-items-center mt-2 lead">
-                    <div>
-                        <h4 className="display-4" style={{ marginBottom: "2rem" }}>
-                            For Lovers of <strong>Monochrome Minimalism</strong>
-                        </h4>
-                        <p style={{ marginBottom: "2rem" }}>
-                            Simple, clean and modern icon pack for your iOS and iPadOS devices.
-                            Icons are designed to be simple and elegant, with a focus on clarity and
-                            consistency.
-                        </p>
-                        <p style={{ marginBottom: "0px" }}>
-                            <em>Try playing with the switches below the iPhone!</em>
-                        </p>
-                    </div>
-                </div>
+        <section className="flex flex-col md:flex-row justify-center">
+            <IPhone />
+            <div className="flex flex-col items-center justify-center w-50 text-center">
+                <h2 className="mb-4 text-[3rem]">
+                    For Lovers of <strong>Monochrome Minimalism</strong>
+                </h2>
+                <p className="mb-8 text-xl">
+                    Simple, clean and modern icon pack for your iOS and iPadOS devices. Icons are
+                    designed to be simple and elegant, with a focus on clarity and consistency.
+                </p>
+                <p className="mb-0 text-xl text-zinc-600 dark:text-zinc-400">
+                    Try playing with the switches below the iPhone!
+                </p>
             </div>
         </section>
     );
