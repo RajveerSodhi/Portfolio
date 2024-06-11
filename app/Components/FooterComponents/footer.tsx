bb333e360ab1a427145601aaab5059ed418d29e0
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-10 22:21:12 -0700

    added edge blur to footer

--- app/Components/FooterComponents/footer.tsx
+++ app/Components/FooterComponents/footer.tsx
@@ -4,7 +4,7 @@ import FooterSocial from "./footer_social";
 
 export default function Footer() {
     return (
-        <div className="p-3 pt-4 mt-60 text-[#777777] d-flex justify-content-between flex-col md:flex-row blurry">
+        <div className="p-3 pt-4 mt-60 text-[#777777] d-flex justify-content-between flex-col md:flex-row blurry blurry-footer-edge">
             <FooterInfo />
             <FooterSocial />
             <FooterNavbar />
