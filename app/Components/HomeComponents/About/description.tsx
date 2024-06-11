6e2db440f38caa0c24f098cadb49d6e0b028177a
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-10 23:40:10 -0700

    removed '

--- app/Components/HomeComponents/About/description.tsx
+++ app/Components/HomeComponents/About/description.tsx
@@ -2,7 +2,7 @@ export default function Description() {
     return (
         <section className="text-center w-[75%] lg:w-[60%] text-xl leading-loose mt-0">
             <p className="appear-animated mb-4">
-                I'm Rajveer Sodhi, a final-year Computer Science Honours student at the{" "}
+                I&apos;m Rajveer Sodhi, a final-year Computer Science Honours student at the{" "}
                 <span className="highlight">University of British Columbia</span>, also minoring in
                 Data Science. I am passionate about research in Computer Science and love being an
                 active member of my community. I am conducting research on innovative interaction
@@ -19,9 +19,9 @@ export default function Description() {
                 leadership, and agile programming practices.
             </p>
             <p className="appear-animated mb-4">
-                I am always eager to connect with like-minded individuals, whether it’s to discuss
-                the latest cars and smartphones, cinema, or potential collaborations. Feel free to
-                reach out to me — I would love to chat and explore new opportunities!
+                I am always eager to connect with like-minded individuals, whether it&apos;s to
+                discuss the latest cars and smartphones, cinema, or potential collaborations. Feel
+                free to reach out to me — I would love to chat and explore new opportunities!
             </p>
         </section>
     );
--- app/Components/HomeComponents/Contact/contact.tsx
+++ app/Components/HomeComponents/Contact/contact.tsx
@@ -6,7 +6,7 @@ import ContactButton from "./contact_button";
 export default function Contact() {
     return (
         <section className="appear-animated flex flex-col justify-center items-center" id="contact">
-            <h1 className="title">Let's Get in Touch</h1>
+            <h1 className="title">Let&apos;s Get in Touch</h1>
             <div className="appear-animated flex flex-col md:flex-row gap-4 mb-12">
                 <ContactButton href="mailto:rajveersodhi03@gmail.com" text="Mail" />
                 <ContactButton href="https://www.linkedin.com/in/rajveersodhi/" text="LinkedIn" />
