7c15c214ade3f63604b97f828fc7a6c893e93a1f
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-19 20:24:35 -0700

    added description highlights back

--- app/Components/HomeComponents/About/description.tsx
+++ app/Components/HomeComponents/About/description.tsx
@@ -4,20 +4,21 @@ export default function Description() {
     return (
         <section className="text-center w-full md:w-[75%] lg:w-[60%] text-sm md:text-xl leading-loose mt-0">
             <p className="appear-animated mb-4">
-                I&apos;m Rajveer Sodhi, a final-year Computer Science Honours student at the
-                University of British Columbia, also minoring in Data Science. I am passionate about
-                research in Computer Science and love being an active member of my community. I am
-                conducting research on innovative interaction techniques for AR/VR headesets and
-                explored in-vehicle infotainment systems, leveraging tools like C# and Unity. At
-                UBC, I am the President of the Film Society. I have also worked as a Supplemental
-                Learning Leader and Teaching Assistant, and created content for a Bolylwood dance
-                club.
+                I&apos;m Rajveer Sodhi, a final-year Computer Science Honours and Data Science Minor
+                student at the <span className="highlight">University of British Columbia</span>. I
+                am passionate about research in Computer Science and love being an active member of
+                my community. I am conducting research on innovative interaction techniques for
+                AR/VR headesets and explored in-vehicle infotainment systems, leveraging tools like
+                C# and Unity. At UBC, I am the President of the Film Society. I have also worked as
+                a Supplemental Learning Leader and Teaching Assistant, and created content for a
+                Bolylwood dance club.
             </p>
             <p className="appear-animated mb-4">
-                I am currently a Software Development Intern at Data POEM, where I develop APIs to
-                retrieve and transform website analytics data. My experiences and roles as an SWDev
-                have taught me a lot about user-centric design, teamwork and leadership, and agile
-                programming practices.
+                I am currently a{" "}
+                <span className="highlight">Software Development Intern at Data POEM</span>, where I
+                develop APIs to retrieve and transform website analytics data. My experiences and
+                roles as an SWDev have taught me a lot about user-centric design, teamwork and
+                leadership, and agile programming practices.
             </p>
             <p className="appear-animated mb-4">
                 I am always eager to{" "}
