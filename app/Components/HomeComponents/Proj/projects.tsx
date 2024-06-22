65a3844c693022290e64650cd5b448c1514eebc5
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-22 01:25:09 -0700

    added projects filter

--- app/Components/HomeComponents/Proj/projects.tsx
+++ app/Components/HomeComponents/Proj/projects.tsx
@@ -7,17 +7,43 @@ import { useEffect, useState } from "react";
 
 export default function Projects() {
     const [showPlaceholder, setShowPlaceholder] = useState(false);
+    const [selectedTag, setSelectedTag] = useState("all");
+    const filter = ["all", "android", "data", "iOS and macOS", "ML", "web"];
+
+    const filteredProjects =
+        selectedTag === "all"
+            ? projects
+            : projects.filter((project) => project.tag === selectedTag);
 
     useEffect(() => {
-        setShowPlaceholder(projects.length % 2 != 0);
-    }, [projects]);
+        setShowPlaceholder(filteredProjects.length % 2 != 0);
+    }, [filteredProjects]);
 
     return (
         <section className="appear-animated" id="projects">
             <h1 className="title">Projects</h1>
+
+            <div>
+                <div className="flex flex-wrap justify-center items-center gap-4 pb-8">
+                    {filter.map((tag, index) => (
+                        <button
+                            key={index}
+                            onClick={() => setSelectedTag(tag)}
+                            className={`cursor-none rounded-full text-center items-center shadow-lg border-1 border-solid w-auto hover:scale-95 transition-all transition-configuration py-2 px-4 md:py-3 md:px-6 text-sm md:text-base ${
+                                selectedTag === tag
+                                    ? "dark:bg-[#fefefe] bg-[#252525] dark:border-[#e7e7e7] border-[#454545] dark:text-myblack text-mywhite"
+                                    : "bg-[#fefefe] dark:bg-[#252525] border-[#e7e7e7] dark:border-[#454545] text-myblack dark:text-mywhite"
+                            }`}
+                        >
+                            {tag}
+                        </button>
+                    ))}
+                </div>
+            </div>
+
             <Accordion transition transitionTimeout={500}>
                 <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-6">
-                    {projects.map((project, index) => (
+                    {filteredProjects.map((project, index) => (
                         <ProjectComponent key={index} {...project} />
                     ))}
                     {showPlaceholder && <Placeholder />}
--- public/content/projects_content.ts
+++ public/content/projects_content.ts
@@ -4,6 +4,7 @@ export const projects = [
         subtitle: "Chrome Extension",
         link: "Custom-Tudum",
         tech: "JavaScript, HTML, CSS, Manifest V3",
+        tag: "web",
         description: [
             "Developed a Chrome extension with MV3 that allows users to customize the Netflix Tudum sound with their own audio files.",
             "Implemented a user-friendly interface for uploading and managing audio files using the Chrome Storage and Offscreen API."
@@ -14,6 +15,7 @@ export const projects = [
         subtitle: "Personal Website",
         link: "Portfolio",
         tech: "Next.js, Tailwind CSS, TypeScript",
+        tag: "web",
         description: [
             "Created a personal website showcasing my technical skills and work experience. Also added a page showcasing iOS icon designs.",
             "Developed using the Next.ts framework in TypeScript. Implemented the Stripe API for payments."
@@ -24,6 +26,7 @@ export const projects = [
         subtitle: "E-Learning Platform",
         link: "EduPool",
         tech: "Python, HTML, CSS, JavaScript, SQL",
+        tag: "web",
         description: [
             "Collaborated in developing a Flask-based e-learning platform, employing Agile methodologies, CI/CD practices, UML for system design, and Test-Driven Development (TDD) with Selenium for automated testing and deployment to ensure quality and performance.",
             "Implemented core features like user authentication, course management, discussion portals, assignment submissions, and grading."
@@ -34,6 +37,7 @@ export const projects = [
         subtitle: "ASL to English Converter",
         link: "Gestura",
         tech: "Python, HTML, CSS, JavaScript",
+        tag: "ML",
         description: [
             "BC Hacks 5.0 Hackathon Winner",
             "Led a cross-functional team in developing a full-stack website, using Node.js to translate American Sign Language into English, integrating a gesture-recognition machine learning model developed with Keras and OpenCV.",
@@ -45,6 +49,7 @@ export const projects = [
         subtitle: "Ride-Share App Prototype",
         link: "TransitMate",
         tech: "Java, XML",
+        tag: "android",
         description: [
             "Engineered a high-fidelity prototype for a ride-sharing app in Android Studio, seamlessly integrating the Google Maps API.",
             "Applied HCI fundamentals by conducting multiple rounds of requirements gathering and user testing to refine the user experience.",
@@ -56,6 +61,7 @@ export const projects = [
         subtitle: "Flight Tracker Menubar App",
         link: "FlightBar",
         tech: "Swift",
+        tag: "iOS and macOS",
         description: [
             "Developed a macOS menubar app using Swift that displays real-time flight data.",
             "Utilized the FlightAware API to fetch flight data and display it in the menubar.",
