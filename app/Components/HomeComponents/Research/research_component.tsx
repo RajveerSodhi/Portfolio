3831305276271893f1dbc881eb0ad3890a944be8
RajveerSodhi <rajveersodhi03@gmail.com>
2024-12-15 16:12:06 -0800

    turned research component into a dynamic link

--- app/Components/HomeComponents/Research/research_component.tsx
+++ app/Components/HomeComponents/Research/research_component.tsx
@@ -1,23 +1,44 @@
 "use client";
 import Tilt from "react-parallax-tilt";
 
-export default function ResearchComponent(props: { title: string; status: string }) {
-    const { title, status } = props;
+export default function ResearchComponent(props: { title: string; status: string; link: string }) {
+    const { title, status, link } = props;
+    const hasLink = link !== "";
+    const Wrapper = hasLink ? "a" : "div";
 
     return (
         <Tilt
             tiltReverse={false}
-            tiltMaxAngleX={2}
+            tiltMaxAngleX={3}
             tiltMaxAngleY={2}
             gyroscope={true}
-            className="appear-animated cursor-research"
+            className={`appear-animated ${hasLink ? "cursor-website" : "cursor-research"}`}
         >
-            <div className="cursor-research py-6 px-4 h-full flex flex-col rounded-3xl max-w-[28rem] w-auto hover:scale-[1.01] transition-transform card">
-                <h3 className="cursor-research mb-4 text-lg md:text-2xl grow">{title}</h3>
-                <h5 className="cursor-research text-base md:text-lg text-zinc-500 dark:text-zinc-400 hover:text-myblack dark:hover:text-mywhite transition-color transition-configuration">
+            <Wrapper
+                {...(hasLink && {
+                    href: link,
+                    target: "_blank",
+                    rel: "noopener noreferrer",
+                })}
+                className={`${
+                    hasLink ? "cursor-website" : "cursor-research"
+                } py-6 px-4 h-full flex flex-col rounded-3xl max-w-[28rem] w-auto hover:scale-[1.01] transition-transform card`}
+            >
+                <h3
+                    className={`${
+                        hasLink ? "cursor-website" : "cursor-research"
+                    } mb-4 text-lg md:text-2xl grow`}
+                >
+                    {title}
+                </h3>
+                <h5
+                    className={`${
+                        hasLink ? "cursor-website" : "cursor-research"
+                    } text-base md:text-lg text-zinc-500 dark:text-zinc-400 hover:text-myblack dark:hover:text-mywhite transition-color transition-configuration`}
+                >
                     {status}
                 </h5>
-            </div>
+            </Wrapper>
         </Tilt>
     );
 }
--- public/content/research_content.ts
+++ public/content/research_content.ts
@@ -1,10 +1,12 @@
 export const research = [
     {
         title: "Comparison of Unencumbered Interaction Techniques in Head Mounted Displays",
-        status: "ISS 2024"
+        status: "ISS 2024",
+        link: "https://doi.org/10.1145/3698146"
     },
     {
         title: "Analysing Unencumbered Interaction Techniques in Head Mounted Displays while Walking",
-        status: "In Progress"
+        status: "In Progress",
+        link: ""
     }
 ]
