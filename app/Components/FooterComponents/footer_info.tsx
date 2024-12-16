d3edb7c4b79dd7a822f7f0e40b9fbfd0cedde65e
RajveerSodhi <rajveersodhi03@gmail.com>
2024-12-15 16:53:52 -0800

    updated footer info

--- app/Components/FooterComponents/footer_info.tsx
+++ app/Components/FooterComponents/footer_info.tsx
@@ -3,7 +3,7 @@ export default function FooterInfo() {
         <span className="flex flex-col xl:flex-row items-center text-center justify-center md:min-w-60">
             <span className="footer-info">Made with ❤️ by Rajveer Sodhi</span>
             <span className="mx-2 hidden xl:inline-block w-[2px] h-[60%] bg-[#777777]"></span>
-            <span className="footer-info">Last Updated: Sep 26, 2024</span>
+            <span className="footer-info">Last Updated: Dec 15, 2024</span>
         </span>
     );
 }
