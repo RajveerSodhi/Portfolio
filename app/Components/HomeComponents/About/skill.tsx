1b5375444fd14bce0bdf2933e5320fc7d7888980
RajveerSodhi <rajveersodhi03@gmail.com>
2025-02-09 07:16:11 -0800

    minor skill UI fix

--- app/Components/HomeComponents/About/skill.tsx
+++ app/Components/HomeComponents/About/skill.tsx
@@ -2,7 +2,7 @@ export default function Skill(props: { name: string }) {
     const { name } = props;
 
     return (
-        <div className="skill py-2 m-1 md:py-3 px-4 md:m-2 w-auto hover:shadow-xl hover:scale-105 rounded-full transition-all card">
+        <div className="skill py-2 m-1 md:py-3 px-4 md:m-2 w-auto hover:shadow-xl hover:scale-105 hover:text-white rounded-full transition-all card">
             {name}
         </div>
     );
