144b4bca4f5ffeaec532effa32394222e158875e
RajveerSodhi <rajveersodhi03@gmail.com>
2025-02-09 03:46:40 -0800

    description update

--- app/Components/HomeComponents/About/description.tsx
+++ app/Components/HomeComponents/About/description.tsx
@@ -4,10 +4,9 @@ export default function Description() {
     return (
         <section className="text-center w-full md:w-[75%] lg:w-[60%] text-sm md:text-xl leading-loose mt-0">
             <p className="appear-animated mb-4">
-                I am pursuing a <em>CS Honours + DS Minor</em> at UBC. I am passionate about
-                research in HCI and love being an active member of my community. My SWDev
-                experiences have taught me a lot about user-centric design, teamwork, and agile
-                practices.
+                I am pursuing a <em>CS Honours</em> at UBC. I am passionate about research in HCI
+                and love being an active member of my community. My SWDev experiences have taught me
+                a lot about user-centric design, teamwork, and agile practices.
             </p>
 
             <p className="appear-animated mb-4">
