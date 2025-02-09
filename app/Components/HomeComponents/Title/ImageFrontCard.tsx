e94bf8ef5852174b37b7a658ab97c0af94903b9f
RajveerSodhi <rajveersodhi03@gmail.com>
2025-02-09 06:34:00 -0800

    revamped title card

--- app/Components/HomeComponents/Title/ImageFrontCard.tsx
+++ app/Components/HomeComponents/Title/ImageFrontCard.tsx
@@ -1,30 +1,36 @@
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
 
 export default function FrontCard() {
-    const theme = useTheme();
-
-    const [myPhotoSrc, setMyPhotoSrc] = useState("/images/myPhoto/light.webp");
+    const [myPhotoSrc, setMyPhotoSrc] = useState(0);
+    let hoverTimer = null;
 
     useEffect(() => {
-        if (theme.resolvedTheme === "dark") {
-            setMyPhotoSrc("/images/myPhoto/dark.webp");
-        } else {
-            setMyPhotoSrc("/images/myPhoto/light.webp");
-        }
-    }, [theme.resolvedTheme]);
+        setMyPhotoSrc(Math.ceil(Math.random() * 12));
+    }, []);
+
+    function handleMouseEnter() {
+        hoverTimer = setTimeout(() => {
+            setMyPhotoSrc(Math.ceil(Math.random() * 12));
+        }, 800);
+    }
+
+    function handleMouseLeave() {
+        clearTimeout(hoverTimer);
+    }
 
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center shadow-lg rounded-[2.8rem] overflow-hidden bg-gradient-to-t from-[#3749A4] to-[#546FFF] dark:from-[#A15F2F] dark:to-[#FF964B]">
             <Image
-                className="shadow-lg rounded-[2.8rem] aspect-square h-[300px] w-[300px] max-h-[300px] max-w-[300px] select-none"
-                src={myPhotoSrc}
+                className="aspect-square h-[300px] w-[300px] max-h-[300px] max-w-[300px] select-none"
+                src={`/images/headerPhoto/img${myPhotoSrc}.webp`}
                 width={300}
                 height={300}
                 priority={true}
-                alt="Rajveer Sodhi Headshot"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                alt="Rajveer Sodhi Header Photo"
             />
         </div>
     );
