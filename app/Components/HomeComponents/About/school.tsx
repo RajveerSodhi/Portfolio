a13a054205e989af25d50400eec6fb81b3add4a7
RajveerSodhi <rajveersodhi03@gmail.com>
2025-09-06 05:04:17 -0400

    edit education

--- app/Components/HomeComponents/About/school.tsx
+++ app/Components/HomeComponents/About/school.tsx
@@ -17,15 +17,17 @@ export default function School(props: {
             tiltMaxAngleX={3}
             tiltMaxAngleY={2}
             gyroscope={true}
-            className={`appear-animated cursor-school flex flex-row gap-4 justify-center items-center`}
+            className={`hover:scale-[1.02] transition-transform rounded-3xl mt-[1.3rem] md:mt-[2.5rem] min-w-full w-auto flex flex-col justify-center appear-animated card cursor-school`}
         >
-            <Image
-                src={`/images/education_photos/${tag}.webp`}
-                alt={`University Emblem - ${name}`}
-                width={100}
-                height={100}
-                className="mb-4 select-none shadow-md hover:shadow-xl rounded-full object-cover object-center hover:scale-[1.01] transition-configuration transition-all"
-            />
+            <div className="cursor-school bg-mywhite w-auto h-auto min-w-[5.4rem] min-h-[5.4rem] aspect-square p-1 rounded-full flex items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-all transition-configuration">
+                <Image
+                    src={`/images/education_photos/${tag}.webp`}
+                    alt={`University Emblem - ${name}`}
+                    width={40}
+                    height={40}
+                    className="cursor-school min-h-[40px] min-w-[40px] object-cover"
+                />
+            </div>
             <div className="flex items-center justify-start flex-col">
                 <h4>{name}</h4>
                 <p>
