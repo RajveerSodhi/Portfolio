44554de0977e2754770f4b3c82c8682fbc0b0f2b
RajveerSodhi <rajveersodhi03@gmail.com>
2025-09-06 05:08:50 -0400

    added education styling

--- app/Components/HomeComponents/About/school.tsx
+++ app/Components/HomeComponents/About/school.tsx
@@ -17,7 +17,7 @@ export default function School(props: {
             tiltMaxAngleX={3}
             tiltMaxAngleY={2}
             gyroscope={true}
-            className={`hover:scale-[1.02] transition-transform rounded-3xl mt-[1.3rem] md:mt-[2.5rem] min-w-full w-auto flex flex-col justify-center appear-animated card cursor-school`}
+            className={`hover:scale-[1.02] flex-row p-6 transition-transform rounded-3xl mt-[1.3rem] md:mt-[2.5rem] min-w-full w-auto flex justify-start items-center appear-animated card cursor-school`}
         >
             <div className="cursor-school bg-mywhite w-auto h-auto min-w-[5.4rem] min-h-[5.4rem] aspect-square p-1 rounded-full flex items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-all transition-configuration">
                 <Image
@@ -28,12 +28,14 @@ export default function School(props: {
                     className="cursor-school min-h-[40px] min-w-[40px] object-cover"
                 />
             </div>
-            <div className="flex items-center justify-start flex-col">
-                <h4>{name}</h4>
-                <p>
+            <div className="flex items-start justify-center flex-col">
+                <h2 className="cursor-school text-xl md:text-3xl">{name}</h2>
+                <h2 className="cursor-school text-base md:text-xl text-zinc-600 dark:text-zinc-300">
                     {degree} {year}
+                </h2>
+                <p className="md:hover:text-myblack md:dark:hover:text-mywhite transition-color transition-configuration">
+                    {location}
                 </p>
-                <p>{location}</p>
             </div>
         </Tilt>
     );
